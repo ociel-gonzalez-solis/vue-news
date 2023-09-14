902c672773f3073d769b461d3f0819040742528f
ociel-gonzalez-solis <ocielgonzalezsolis@hotmail.com>
2023-09-13 17:18:47 -0700

    EntryNewForm ready.

--- src/modules/daybook/components/Fab.vue
+++ src/modules/daybook/components/Fab.vue
@@ -1,21 +1,28 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <button class="btn btn-primary"><i class="fa fa-2x fa-plus">{{ "" }}</i></button>
+  <button class="btn btn-primary">
+    <i class="fa fa-2x" :class="icon">{{ "" }}</i>
+  </button>
 </template>
 
 <script>
 export default {
-
-}
+  props: {
+    icon: {
+      type   : String,
+      default: 'fa-plus'
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
- button{
-    position     : fixed;
-    bottom       : 20px;
-    right        : 20px;
-    width        : 80px;
-    height       : 80px;
-    border-radius: 100%;
- }
+button {
+  position     : fixed;
+  bottom       : 20px;
+  right        : 20px;
+  width        : 80px;
+  height       : 80px;
+  border-radius: 100%;
+}
 </style>
--- src/modules/daybook/views/EntryView.vue
+++ src/modules/daybook/views/EntryView.vue
@@ -13,15 +13,58 @@
       </button>
 
       <button class="btn btn-primary mx-2">
-        SUbir FOto
+        Subir Foto
         <i class="fa fa-upload"></i>
       </button>
     </div>
   </div>
+
+  <hr />
+  <div class="d-flex flex-column px-3 h-75">
+    <textarea
+      name=""
+      id=""
+      cols="30"
+      rows="10"
+      placeholder="Que sucedio hoy?"
+    ></textarea>
+  </div>
+
+  <Fab icon="fa-save" />
+
+  <img
+    src="https://upload.wikimedia.org/wikipedia/commons/b/b3/Nature_.jpg"
+    alt="ebtry-point"
+    class="img-thumbnail"
+  />
 </template>
 
 <script>
-export default {};
+import { defineAsyncComponent } from "vue";
+
+export default {
+  components: {
+    Fab: defineAsyncComponent(() => import("../components/Fab.vue")),
+  },
+};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+textarea {
+  font-size: 20px;
+  border: none;
+  height: 100%;
+
+  &:focus {
+    outline: none;
+  }
+}
+
+img {
+  width: 200px;
+  position: fixed;
+  bottom: 150px;
+  right: 20px;
+  box-shadow: 0px 5px 10px rgba($color: #000000, $alpha: 0.2);
+}
+</style>
