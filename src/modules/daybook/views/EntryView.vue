ce7643ddb9717363403706ed09df13683b83f09a
ociel-gonzalez-solis <ocielgonzalezsolis@hotmail.com>
2023-10-06 17:19:02 -0700

    EntryView reform.

--- src/modules/daybook/views/EntryView.vue
+++ src/modules/daybook/views/EntryView.vue
@@ -40,37 +40,6 @@
     class="img-thumbnail"
   />
   </template>
-  <div class="entry-title d-flex justify-content-between p-2">
-    <div>
-      <span class="text-success fs-3 fw-bold">{{ day }}</span>
-      <span class="mx-1 fs-3">{{ month }}</span>
-      <span class="mx-2 fs-4 fw-light">{{ year }}</span>
-    </div>
-
-    <div>
-      <button class="btn btn-danger mx-2">
-        Borrar
-        <i class="fa fa-trash-alt"></i>
-      </button>
-
-      <button class="btn btn-primary mx-2">
-        Subir Foto
-        <i class="fa fa-upload"></i>
-      </button>
-    </div>
-  </div>
-
-  <hr />
-  <div class="d-flex flex-column px-3 h-75">
-    <textarea
-      name=""
-      id=""
-      cols="30"
-      rows="10"
-      placeholder="Que sucedio hoy?"
-      v-model="entry.text"
-    ></textarea>
-  </div>
 
   <Fab icon="fa-save" />
 
